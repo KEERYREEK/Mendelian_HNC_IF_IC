627838d823b86681f1fe4ad89b26734a751bcdfb
ZHU GUIXIN <58208025+384257097@users.noreply.github.com>
2026-03-17 01:49:50 +0800

    Update FDRandBonferroni.R

--- Rscript/FDRandBonferroni.R
+++ Rscript/FDRandBonferroni.R
@@ -5,14 +5,12 @@ setwd("circ_plot/")
 data <- read.csv("HNC2IF.csv", stringsAsFactors = FALSE)
 
 # 2. 确认你的IVW p值所在列名 
-# 根据您上传的文件片段，列名似乎为 "p"。如果是 "IVW_p"，请将下方的 "p" 修改为 "IVW_p"
 p_col_name <- "p" 
 
 # 3. 设定总体的检验次数 (IC个数)
 total_tests <- 91
 
 # 4. 计算 Bonferroni 校正
-# 使用 p.adjust 函数，并强制指定 n = total_tests
 data$Bonferroni_p <- p.adjust(data[[p_col_name]], method = "bonferroni", n = total_tests)
 
 # 5. 计算 FDR (False Discovery Rate, 通常即为 BH 方法) 校正
